c2fe686915937fa21fe0e67c04d6c628286fc37f
Jules Bertholet <jules.bertholet@gmail.com>
2021-03-31 15:54:24 -0400

    Use yarn from Yarn's deb repo
    Avoids conflicts with other Yunohost apps that do the same

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="nodejs npm libpng16-16 libpng-dev pkg-config autoconf libtool build-essential nasm libx11-dev libxkbfile-dev yarnpkg"
+pkg_dependencies="nodejs npm libpng16-16 libpng-dev pkg-config autoconf libtool build-essential nasm libx11-dev libxkbfile-dev"
 
 #=================================================
 # PERSONAL HELPERS
