ee41044ec0eafd3c626d2ae7d20491f09fc0b2aa
Jules Bertholet <jules.bertholet@gmail.com>
2021-04-17 21:32:11 -0400

    Minor fixes

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -11,6 +11,26 @@ pkg_dependencies="nodejs npm libpng16-16 libpng-dev pkg-config autoconf libtool 
 # PERSONAL HELPERS
 #=================================================
 
+function build_node_app {
+	ynh_exec_warn_less ynh_install_nodejs --nodejs_version=10
+	ynh_use_nodejs
+	node_PATH=$nodejs_path:$(sudo -u $app sh -c 'echo $PATH')
+
+	pushd $final_path || ynh_die
+		chown -R $app:$app $final_path
+		sudo -u $app touch $final_path/.yarnrc
+		sudo -u $app "env PATH=$node_PATH" yarn --cache-folder $final_path/yarn-cache --use-yarnrc $final_path/.yarnrc install 2>&1
+		chown -R root:root $final_path
+	popd || ynh_die
+}
+
+function set_permissions {
+	chown -R root:$app $final_path
+	chmod -R g=u,g-w,o-rwx $final_path
+	chown -R $app:$app $data_path
+	chmod -R g=u,g-w,o-rwx $data_path
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
