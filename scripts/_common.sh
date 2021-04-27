aad56a4e94315ccad3720cc2950bb0729d8c7f8f
Jules Bertholet <jules.bertholet@gmail.com>
2021-04-27 16:56:53 -0400

    Make Node.js build step more efficient

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,24 +14,28 @@ pkg_dependencies="nodejs npm libpng16-16 libpng-dev pkg-config autoconf libtool 
 function set_node_vars {
 	ynh_exec_warn_less ynh_install_nodejs --nodejs_version=12
 	ynh_use_nodejs
-	node_path=$nodejs_path:$(sudo -u $app sh -c 'echo $PATH')
+	node_path="$nodejs_path:$(sudo -u $app sh -c 'echo $PATH')"
 }
 
 function build_node_app {
 	set_node_vars
-	pushd $final_path || ynh_die
-		chown -R $app:$app $final_path
-		sudo -u $app touch $final_path/.yarnrc
-		sudo -u $app "env PATH=$node_path" yarn --cache-folder $final_path/yarn-cache --use-yarnrc $final_path/.yarnrc install 2>&1
-		chown -R root:root $final_path
-	popd || ynh_die
+
+	cat >> "$final_path/package.json" <<< "$(grep electron $final_path/package.json)"
+	pushd "$final_path"
+		chown -R $app:$app "$final_path"
+		sudo -u $app touch "$final_path/.yarnrc"
+		sudo -u $app "env PATH=$node_path" yarn --cache-folder "$final_path/yarn-cache" --use-yarnrc "$final_path/.yarnrc" install --production 2>&1
+		chown -R root:root "$final_path"
+	popd
+
+	set_permissions
 }
 
 function set_permissions {
-	chown -R root:$app $final_path
-	chmod -R g=u,g-w,o-rwx $final_path
-	chown -R $app:$app $data_path
-	chmod -R g=u,g-w,o-rwx $data_path
+	chown -R root:$app "$final_path"
+	chmod -R g=u,g-w,o-rwx "$final_path"
+	chown -R $app:$app "$data_path"
+	chmod -R g=u,g-w,o-rwx "$data_path"
 }
 
 #=================================================
