13ea3f4a1d2b53bbf07450831040f50cf4eb9a52
Jules Bertholet <jules.bertholet@quoi.xyz>
2022-01-02 12:45:04 -0500

    Update to v0.48.9

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,7 +12,11 @@ pkg_dependencies="nodejs npm libpng16-16 libpng-dev pkg-config autoconf libtool 
 #=================================================
 
 function set_node_vars {
-	ynh_exec_warn_less ynh_install_nodejs --nodejs_version=14
+	nodejs_version=$(ynh_app_setting_get --app=$app --key=nodejs_version)
+	if [ $nodejs_version -ne 16 ]; then 
+		ynh_exec_warn_less ynh_remove_nodejs
+	fi
+	ynh_exec_warn_less ynh_install_nodejs --nodejs_version=16
 	ynh_use_nodejs
 	node_path="$nodejs_path:$(sudo -u $app sh -c 'echo $PATH')"
 }
@@ -25,6 +29,7 @@ function build_node_app {
 	pushd "$final_path"
 		chown -R $app:$app "$final_path"
 		sudo -u $app touch "$final_path/.yarnrc"
+		sudo -u $app env "PATH=$node_path" yarn --cache-folder "$final_path/yarn-cache" --use-yarnrc "$final_path/.yarnrc" import 2>&1
 		sudo -u $app env "PATH=$node_path" yarn --cache-folder "$final_path/yarn-cache" --use-yarnrc "$final_path/.yarnrc" install --production 2>&1
 		chown -R root:root "$final_path"
 	popd
