ca9941de79ac92d187298b2d62cc342753bf6a4d
ericgaspar <junk.eg@free.fr>
2022-03-06 08:14:53 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,8 @@
 # COMMON VARIABLES
 #=================================================
 
+nodejs_version=16
+
 # dependencies used by the app
 pkg_dependencies="nodejs npm libpng16-16 libpng-dev pkg-config autoconf libtool build-essential nasm libx11-dev libxkbfile-dev"
 
@@ -11,18 +13,18 @@ pkg_dependencies="nodejs npm libpng16-16 libpng-dev pkg-config autoconf libtool 
 # PERSONAL HELPERS
 #=================================================
 
-function set_node_vars {
-	nodejs_version=$(ynh_app_setting_get --app=$app --key=nodejs_version)
-	if [ $nodejs_version -ne 16 ]; then 
-		ynh_exec_warn_less ynh_remove_nodejs
-	fi
-	ynh_exec_warn_less ynh_install_nodejs --nodejs_version=16
-	ynh_use_nodejs
-	node_path="$nodejs_path:$(sudo -u $app sh -c 'echo $PATH')"
-}
+# function set_node_vars {
+# 	nodejs_version=$(ynh_app_setting_get --app=$app --key=nodejs_version)
+# 	if [ $nodejs_version -ne 16 ]; then 
+# 		ynh_exec_warn_less ynh_remove_nodejs
+# 	fi
+# 	ynh_exec_warn_less ynh_install_nodejs --nodejs_version=16
+# 	ynh_use_nodejs
+# 	node_path="$nodejs_path:$(sudo -u $app sh -c 'echo $PATH')"
+# }
 
 function build_node_app {
-	set_node_vars
+	#set_node_vars
 
 	grep -v electron "$final_path/package.json" > "$final_path/server-package.json"
 	mv "$final_path/server-package.json" "$final_path/package.json"
