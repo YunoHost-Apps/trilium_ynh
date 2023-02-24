d35be6111dacf2356582adef10067ed39c6f3922
John Schmidt <john@schmidthaus.rocks>
2023-02-24 11:00:57 -0800

    Switch to packaging v2 and upgrade to v0.58.8
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,9 +6,6 @@
 
 nodejs_version=16
 
-# dependencies used by the app
-pkg_dependencies="libpng16-16 libpng-dev pkg-config autoconf libtool build-essential nasm libx11-dev libxkbfile-dev"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
